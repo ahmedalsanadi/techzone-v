b9eb7949174629e021c66d18b8e3703daa3c1b08
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2026-01-03 03:52:47 +0300

    feat(ui): integrate SubHeader component into LandingPage and update translations for order type selection

--- src/components/pages/landing-page/LandingPage.tsx
+++ src/components/pages/landing-page/LandingPage.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Navbar from '@/components/layouts/Navbar';
+import SubHeader from '@/components/layouts/SubHeader';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -9,10 +10,12 @@ export default function LandingPage() {
     return (
         <main>
             {/*-------- Red bg header ----------- */}
-            <nav className="bg-libero-red h-[100px] ">
+            <nav className="bg-libero-red h-[140px] ">
                 <div className="relative container mx-auto p-4">
                     {/*-- Navbar---- */}
                      <Navbar />  
+
+                     <SubHeader />
                 </div>
             </nav>
         </main>
