9cea64ee7f0f81a1d9092378bc16608be1ec5838
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2026-01-05 04:52:05 +0300

    feat(getQueryClient): add utility function to create a configured QueryClient for React Query

---
+++ src/lib/getQueryClient.ts
@@ -0,0 +1,20 @@
+import {
+    QueryClient,
+    defaultShouldDehydrateQuery,
+} from '@tanstack/react-query';
+
+export const getQueryClient = () =>
+    new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 60 * 1000,
+            },
+            dehydrate: {
+                // per default, only successful queries are dehydrated,
+                // this can be configured (e.g. to dehydrate failed queries as well)
+                shouldDehydrateQuery: (query) =>
+                    defaultShouldDehydrateQuery(query) ||
+                    query.state.status === 'pending',
+            },
+        },
+    });
