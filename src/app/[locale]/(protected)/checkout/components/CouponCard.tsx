0d102f9b7bdbd32eaabe14fb2d0f07f93ebb90eb
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2026-01-22 04:54:47 +0300

    fix(coupon): Remove unnecessary whitespace in CouponCard input class for improved consistency in styling

--- src/app/[locale]/(protected)/checkout/components/CouponCard.tsx
+++ src/app/[locale]/(protected)/checkout/components/CouponCard.tsx
@@ -7,7 +7,7 @@ export default function CouponCard() {
                 <input
                     type="text"
                     placeholder="كود الكوبون"
-                    className="w-full sm:w-60  border border-gray-300 rounded-lg px-4 py-2.5 focus:ring-2 focus:ring-red-600"
+                    className="w-full sm:w-60 border border-gray-300 rounded-lg px-4 py-2.5 focus:ring-2 focus:ring-red-600"
                 />
                 <button className="px-6 py-2.5 border border-red-600 text-red-600 rounded-lg text-lg font-semibold hover:bg-red-50 whitespace-nowrap">
                     تحقق
