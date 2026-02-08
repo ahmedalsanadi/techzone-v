2ba3dcebf965c25829da44de565dea25236d774b
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2026-02-08 22:52:16 +0300

    fix(logo): update logo dimensions and restore brand name display for improved branding consistency

--- src/components/layouts/LogoImage.tsx
+++ src/components/layouts/LogoImage.tsx
@@ -10,7 +10,7 @@ interface LogoProps {
 export default function LogoImage({ brandName, brandLogo }: LogoProps) {
     return (
         <Link href="/" className="flex items-center gap-1.5 md:gap-2">
-            <div className="relative w-6 h-6 md:w-8 md:h-8">
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
                 {brandLogo && (
                     <Image
                         src={brandLogo}
@@ -21,9 +21,8 @@ export default function LogoImage({ brandName, brandLogo }: LogoProps) {
                     />
                 )}
             </div>
-            <p className="text-white text-xl md:text-[24px] font-medium leading-none">
-                {/* {brandName} */}
-                Fitoro
+            <p className="text-white text-lg md:text-[22px] font-medium leading-none">
+                {brandName}
             </p>
         </Link>
     );
