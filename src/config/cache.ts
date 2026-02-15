101ba751fd21b746a3bfdefe8222b55234f0e2ac
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2026-02-15 09:35:54 +0300

    feat: Add cache strategy and tags configuration for various application data.

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -5,22 +5,22 @@
  */
 export const CACHE_STRATEGY = {
     // CMS Pages
-    CMS_PAGES_LIST: 0, // 24 hours
-    CMS_PAGE_SINGLE: 0, // 1 week
+    CMS_PAGES_LIST: 1000, // 24 hours
+    CMS_PAGE_SINGLE: 1000, // 1 week
 
     // Store Configuration
-    STORE_CONFIG: 0, // 24 hours
+    STORE_CONFIG: 1000, // 24 hours
 
     // Categories & Collections
-    CATEGORIES_TREE: 0, // 1 hour
-    COLLECTIONS: 0, // 1 hour
+    CATEGORIES_TREE: 1000, // 1 hour
+    COLLECTIONS: 1000, // 1 hour
 
     // Products
-    PRODUCTS_LIST:0, // 1 hour
-    PRODUCT_SINGLE: 0, // 1 hour
+    PRODUCTS_LIST:1000, // 1 hour
+    PRODUCT_SINGLE: 1000, // 1 hour
 
     // Static Data
-    COUNTRIES_CITIES: 0, // 24 hours
+    COUNTRIES_CITIES: 1000, // 24 hours
 };
 
 /**
